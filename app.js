d676577cbcae1d9fbd0abf9ad2a9015d03ec2c1e
Alex Wiley <alex@alexwiley.co.uk>
2017-10-18 17:05:43 +0100

    Page module started on

--- app.js
+++ app.js
@@ -3,8 +3,14 @@
 'use strict';
 
 const fs = require('fs'),
-    prompt = require('prompt'),
-    colors = require("colors");
+    colors = require("colors"),
+    inquirer = require('inquirer'),
+    argv = require( 'argv' ),
+    logger = require('./lib/sys/resultLogger');
+
+//Import app modules
+const addComponent = require('./lib/util/addComponent'),
+    addPage = require('./lib/util/addPage');
 
 /**
  * Default options input for program to run.
@@ -16,10 +22,32 @@ let options = {
     compositePath: './src/js/jet-composites'
 };
 
+const setupOpts = () => {
+    argv.option({
+        name: 'option',
+        short: 'o',
+        type: 'string',
+        description: 'Defines an option for your script',
+        example: "'script --opiton=value' or 'script -o value'"
+    });
+}
+
+const parseArgs = () => {
+    // setupOpts();
+    let command = argv.run().targets[0] ;
+    switch (command){
+        case 'component':
+            addComponent.init();
+            break;
+        case 'page':
+            addPage.init();
+            break;
+        default:
+            logger(colors.red(`[ERROR]: ${command} is not a command. Please try again.`), "both", true);
+    }
+};
 
 
-const addComponent = require('./lib/addComponent');
-
-addComponent()
+module.exports = parseArgs();
 
 
